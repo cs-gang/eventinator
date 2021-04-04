1ad95fa75ef22888d40c6620ae27e5956e820db5
Anand Krishna <anandkrishna2312@gmail.com>
2021-04-04 14:12:35 +0400

    Add firebase_admin to dependencies, initialize database

--- onehacks/server.py
+++ onehacks/server.py
@@ -1,4 +1,25 @@
+import asyncio
+import os
+
+from dotenv import find_dotenv, load_dotenv
+import firebase_admin
+from firebase_admin import credentials
 from sanic import Sanic
 
+from onehacks.database import Database
+
+load_dotenv(find_dotenv())
 
 app = Sanic("onehacks")
+app.config.DB_URI = os.environ.get("DB_URI", "sqlite://data.db")
+app.ctx.db = Database(app)
+
+# initializing firebase app
+cred = credentials.Certificate("admin-sdk.json")
+firebase = firebase_admin.initialize_app(cred)
+
+
+@app.before_server_start
+async def connect_db(app: Sanic, loop: asyncio.AbstractEventLoop) -> None:
+    await app.ctx.db.connect()
+    await app.ctx.db.initialize_tables()
