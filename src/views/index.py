25593f56de23fc87168b1d4421590a16be3fc746
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2021-06-26 14:58:59 +0400

    refactor: handle unauthenticatederror

--- src/views/index.py
+++ src/views/index.py
@@ -1,6 +1,7 @@
 from sanic.request import Request
 from sanic.response import html, HTTPResponse
 
+from src.auth import UnauthenticatedError
 from src.forms import LoginForm, SignUpForm
 from src.server import app
 from src.utils import render_page
@@ -14,3 +15,11 @@ async def index(request: Request) -> HTTPResponse:
         app.ctx.env, file="index.html", login_form=login_form, signup_form=signup_form
     )
     return HTTPResponse(output, content_type="text/html")
+
+
+@app.exception(UnauthenticatedError)
+async def redirect_to_login(
+    request: Request, exception: Type[Exception]
+) -> HTTPResponse:
+    output = await render_page(app.ctx.env, file="not-logged-in.html")
+    return html(output)
